6f11bcb1f33d75687e2f32a08ff66b69709a1621
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2021-03-04 15:11:19 -0300

    Adding Exception Handling to the js

--- logic.js
+++ logic.js
@@ -1,10 +1,18 @@
 //function that converts the input into dec and sends to output
 
 function con() {
-    const bin = document.getElementById('bin').value //get the binary number from the user input
-    //TODO: tratamento de entrada
+  const bin = document.getElementById('bin').value //get the binary number from the user input
 
-    const decimal = parseInt(bin, 2); //convert the binary to decimal
+  if (bin.match("^[0-1]{1,}$")) { //se o valor de entrada for um binario
+
+    const decimal = parseInt(Number(bin), 2); //convert the binary to decimal
     document.getElementById('dec').value = decimal; //output the decimal value to the user
-    return true;
+  }
+  else {
+    return alert('Please, enter a binary number');
+  }
+
+  return true;
 };
+
+
