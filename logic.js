8761fe23f7555d53b9b900022589687c17d5a3f6
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2021-03-04 16:12:20 -0300

    changing comments to explain the logic better

--- logic.js
+++ logic.js
@@ -3,13 +3,13 @@
 function con() {
   const bin = document.getElementById('bin').value //get the binary number from the user input
 
-  if (bin.match("^[0-1]{1,}$")) { //se o valor de entrada for um binario
+  if (bin.match("^[0-1]{1,}$")) { //regex to verify if the entry is a binary
 
     const decimal = parseInt(Number(bin), 2); //convert the binary to decimal
     document.getElementById('dec').value = decimal; //output the decimal value to the user
   }
   else {
-    return alert('Please, enter a binary number');
+    return alert('Please, enter a binary number'); //if the entry is not a binary show this message.
   }
 
   return true;
